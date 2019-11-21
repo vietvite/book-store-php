65283fe793f3eed36e2da087d3b392e5f789343d
vietvite <vanviet8367@gmail.com>
2019-11-21 18:00:32 +0700

    Add environment file

---
+++ environment.example.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Rename this file to environment.php
+ * then replace your database variable name
+ */
+$_ENV["username"] = "";
+$_ENV["password"] = "";
+$_ENV["database"] = "db_name";
+$_ENV["url"] = "localhost";
+?>
